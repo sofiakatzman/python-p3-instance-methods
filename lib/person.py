6290b61cb51d4c5cb8b0d5efb49dbed47743742c
Sofia <katzmansof@gmail.com>
2023-05-18 14:16:07 -0400

    finished lab

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
+class Person: 
+    # Class body goes here 
 
-    #Instance method definition
-    pass
+    # Instance method definitions 
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
